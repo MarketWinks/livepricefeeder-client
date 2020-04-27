5281a21a4413236c305b1426d4e5423c68260814
Chocks <chocku.engr@gmail.com>
2020-04-27 09:01:38 +0100

    initial commit

--- src/main/java/com/marketwinks/livepricefeederclient/services/UK_LSE_ClientService.java
+++ src/main/java/com/marketwinks/livepricefeederclient/services/UK_LSE_ClientService.java
@@ -74,7 +74,8 @@ public class UK_LSE_ClientService {
 
 				while (line != null) {
 
-					String macdurl = "https://livemacdcalculator.herokuapp.com" + "/uk_lse_5mins_livemarketmacd/" + line.toString() + "/calc";
+					String macdurl = "https://livemacdcalculator.herokuapp.com" + "/uk_lse_5mins_livemarketmacd/"
+							+ line.toString() + "/calc";
 
 					urlList.add(macdurl);
 
@@ -97,4 +98,245 @@ public class UK_LSE_ClientService {
 		return execution_result;
 
 	}
+
+	@RequestMapping(value = "/UK_LSE/15Mins/PriceFeed/start", method = RequestMethod.GET)
+	public boolean findUK_LSE_15Mins_Price() throws InterruptedException, IOException {
+
+		boolean execution_result = false;
+		List<String> urlList = new ArrayList<String>();
+
+		try {
+			File file = new File("src/main/java/com/marketwinks/livepricefeederclient/services/uk_lse_symbols.txt");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+
+					String priceurl = serverURL + "/uk_lse_15mins_livemarketprice/15Mins/" + line.toString();
+
+					urlList.add(priceurl);
+
+					line = br.readLine();
+				}
+
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < urlList.size(); i++) {
+			UK_LSE_15Mins_Price_ProcessorThread.process(urlList.get(i));
+		}
+
+		execution_result = true;
+		return execution_result;
+
+	}
+
+	@RequestMapping(value = "/UK_LSE/30Mins/PriceFeed/start", method = RequestMethod.GET)
+	public boolean findUK_LSE_30Mins_Price() throws InterruptedException, IOException {
+
+		boolean execution_result = false;
+		List<String> urlList = new ArrayList<String>();
+
+		try {
+			File file = new File("src/main/java/com/marketwinks/livepricefeederclient/services/uk_lse_symbols.txt");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+
+					String priceurl = serverURL + "/uk_lse_30mins_livemarketprice/30Mins/" + line.toString();
+
+					urlList.add(priceurl);
+
+					line = br.readLine();
+				}
+
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < urlList.size(); i++) {
+			UK_LSE_30Mins_Price_ProcessorThread.process(urlList.get(i));
+		}
+
+		execution_result = true;
+		return execution_result;
+
+	}
+
+	@RequestMapping(value = "/UK_LSE/Hourly/PriceFeed/start", method = RequestMethod.GET)
+	public boolean findUK_LSE_Hourly_Price() throws InterruptedException, IOException {
+
+		boolean execution_result = false;
+		List<String> urlList = new ArrayList<String>();
+
+		try {
+			File file = new File("src/main/java/com/marketwinks/livepricefeederclient/services/uk_lse_symbols.txt");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+
+					String priceurl = serverURL + "/uk_lse_hourly_livemarketprice/Hourly/" + line.toString();
+
+					urlList.add(priceurl);
+
+					line = br.readLine();
+				}
+
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < urlList.size(); i++) {
+			UK_LSE_Hourly_Price_ProcessorThread.process(urlList.get(i));
+		}
+
+		execution_result = true;
+		return execution_result;
+
+	}
+
+	@RequestMapping(value = "/UK_LSE/Daily/PriceFeed/start", method = RequestMethod.GET)
+	public boolean findUK_LSE_Daily_Price() throws InterruptedException, IOException {
+
+		boolean execution_result = false;
+		List<String> urlList = new ArrayList<String>();
+
+		try {
+			File file = new File("src/main/java/com/marketwinks/livepricefeederclient/services/uk_lse_symbols.txt");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+
+					String priceurl = serverURL + "/uk_lse_daily_livemarketprice/Daily/" + line.toString();
+
+					urlList.add(priceurl);
+
+					line = br.readLine();
+				}
+
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < urlList.size(); i++) {
+			UK_LSE_Daily_Price_ProcessorThread.process(urlList.get(i));
+		}
+
+		execution_result = true;
+		return execution_result;
+
+	}
+
+	@RequestMapping(value = "/UK_LSE/Weekly/PriceFeed/start", method = RequestMethod.GET)
+	public boolean findUK_LSE_Weekly_Price() throws InterruptedException, IOException {
+
+		boolean execution_result = false;
+		List<String> urlList = new ArrayList<String>();
+
+		try {
+			File file = new File("src/main/java/com/marketwinks/livepricefeederclient/services/uk_lse_symbols.txt");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+
+					String priceurl = serverURL + "/uk_lse_weekly_livemarketprice/Weekly/" + line.toString();
+
+					urlList.add(priceurl);
+
+					line = br.readLine();
+				}
+
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < urlList.size(); i++) {
+			UK_LSE_Weekly_Price_ProcessorThread.process(urlList.get(i));
+		}
+
+		execution_result = true;
+		return execution_result;
+
+	}
+
+	@RequestMapping(value = "/UK_LSE/Monthly/PriceFeed/start", method = RequestMethod.GET)
+	public boolean findUK_LSE_Monthly_Price() throws InterruptedException, IOException {
+
+		boolean execution_result = false;
+		List<String> urlList = new ArrayList<String>();
+
+		try {
+			File file = new File("src/main/java/com/marketwinks/livepricefeederclient/services/uk_lse_symbols.txt");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+
+					String priceurl = serverURL + "/uk_lse_monthly_livemarketprice/Monthly/" + line.toString();
+
+					urlList.add(priceurl);
+
+					line = br.readLine();
+				}
+
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < urlList.size(); i++) {
+			UK_LSE_Monthly_Price_ProcessorThread.process(urlList.get(i));
+		}
+
+		execution_result = true;
+		return execution_result;
+
+	}
+
 }
